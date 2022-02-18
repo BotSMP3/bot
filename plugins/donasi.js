c70624ad4b5a2e23e4e31582a8a72290d503082c
Botsergio <99638282+Botsergio@users.noreply.github.com>
2022-02-18 16:11:05 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi 」
-│ • Indosat [082114499086]
-│ • Gopay [082114499086]
+│ • Indosat [082124137452]
+│ • Gopay [082124137452]
 │ • Saweria [https://saweria.co/botstyle]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6282114499086
+│ > Ingin donasi? Wa.me/6282124137452
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
