649c97350769ef5ab2a4fd97f9e6d7b6bdea429f
Botsergio <99638282+Botsergio@users.noreply.github.com>
2022-03-06 22:05:40 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,20 @@
-let handler = async m => m.reply(`
-
-╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6285846050774
-╰────
-`.trim()) // Tambah sendiri kalo mau
-handler.help = ['donasi']
-handler.tags = ['info']
-handler.command = /^dona(te|si)$/i
-
-module.exports = handler
+(1) LIMIT UNLIMITED UNTUK MENGGUNAKAN BOT SEPUASNYA + PENGGUNA PREMIUM + BISA SPAM KE BOT UNLIMITED = 10 ribu / 30 hari
+            
+(2) SEWA BOT UNTUK 1 GRUP MU + LIMIT UNLIMITED + BISA SPAM KE BOT UNLIMITED = 15 ribu / 30 hari                                    
+                     
+(3) NOMOR MU JADI BOT + BISA SPAM KE BOT UNLIMITED + BANYAK KEUNTUNGAN NYA = 25 ribu / 30 hari
+                
+(4) JADI OWNER BOT + BISA SPAM KE BOT UNLIMITED + BANYAK KEUNTUNGAN NYA = 50 ribu / 30 hari
+                
+                                                                            
+METODE PEMBAYARAN :
+                                                                            
+1. PULSA : 082171240131
+2. GOPAY : 082171240131
+3. OVO : 082171240131
+4. DANA : 082171240131
+                                                                            
+                                                                                            
+❗KHUSUS YG MAU SEWA/ORDER BOT, CHAT OWNER BOT INI : wa.me/6282171240131 atau bisa langsung Chat Bot nya
+                                                                            
+❗NOTE : HARGA PERPANJANG SESUAI HARGA SEWA/ORDER DI AWAL🔥
