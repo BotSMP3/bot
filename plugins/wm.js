e0bca5cbdac32ae718fee86a9a44b78e60467e5c
Botsergio <99638282+Botsergio@users.noreply.github.com>
2022-03-09 08:34:44 +0700

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,40 +1,28 @@
-let { sticker } = require('../lib/sticker')
-let uploadFile = require('../lib/uploadFile')
-let uploadImage = require('../lib/uploadImage')
-let { webp2png } = require('../lib/webp2mp4')
-
-let handler = async (m, { conn, text }) => {
+const {
+	MessageType
+} = require('@adiwajshing/baileys')
+const {
+	sticker
+} = require('../lib/sticker')
+let handler = async (m, {
+	conn,
+	text
+}) => {
 	let stiker = false
 	try {
-		if (!m.quoted) return m.reply('Reply stikernya')
-		await m.reply('Loading...')
-		let [packname, ...author] = text.split`|`
-		author = (author || []).join`|`
-		let q = m.quoted ? m.quoted : m
+		let [packname, ...author] = text.split('|')
+		author = (author || []).join('|')
 		let mime = m.quoted.mimetype || ''
-		if (/webp/.test(mime)) {
-			let img = await q.download()
-			let out = await webp2png(img)
-			if (!img) return m.reply(`Reply stiker dengan perintah ${usedPrefix + command} <packname>|<author>`)
-			stiker = await sticker(0, out, packname || '', author || '')
-		} else if (/image/.test(mime)) {
-			let img = await q.download()
-			let link = await uploadImage(img)
-			if (!img) return m.reply(`Reply gambar dengan perintah ${usedPrefix + command} <packname>|<author>`)
-			stiker = await sticker(0, link, packname || '', author || '')
-		} else if (/video/.test(mime)) {
-			if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
-			let img = await q.download()
-			let link = await uploadFile(img)
-			if (!img) return m.reply(`Reply video dengan perintah ${usedPrefix + command} <packname>|<author>`)
-			stiker = await sticker(0, link, packname || '', author || '')
-		}
+		if (!/webp/.test(mime)) throw 'Reply sticker!'
+		let img = await m.quoted.download()
+		stiker = await sticker(img, false, packname || global.packname, author || global.author)
 	} finally {
-		if (stiker) await conn.sendMessage(m.chat, stiker, 'stickerMessage', { quoted: m })
-		//else return m.reply('Reply stikernya!')
+		if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+			quoted: m
+		})
+		else throw 'Conversion failed'
 	}
 }
-
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
 handler.command = /^wm$/i
